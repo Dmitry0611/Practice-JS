bb9910408d5c79d1050655ee91d52f5cc2e77128
Dmitry <malash4enko@mail.ru>
2025-01-09 18:20:26 +0300

    moved all functions inside the object and added a new function toggleVisibleMyDB

--- script.js
+++ script.js
@@ -1,72 +1,69 @@
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-};
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-              ratingFilm = prompt('На сколько оцените его?', '');
-
-        if (lastFilm != null && ratingFilm != null && lastFilm != '' && ratingFilm != '' && lastFilm.length < 50) {
-            personalMovieDB.movies[lastFilm] = ratingFilm;
-        } else {
-            console.log('error');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
         }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+                  ratingFilm = prompt('На сколько оцените его?', '');
+
+            if (lastFilm != null && ratingFilm != null && lastFilm != '' && ratingFilm != '' && lastFilm.length < 50) {
+                personalMovieDB.movies[lastFilm] = ratingFilm;
+            } else {
+                console.log('error');
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            alert('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            alert('Вы киноман');
+        } else {
+            alert('Произошла ошибка')
+        }
+
+    },
+    showMyDB: function(obj) {
+        if (obj.privat == false) {
+            console.log(personalMovieDB);
+        } else {
+            console.log(`База данных скрыта для вас!`)
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false
+        } else {
+            personalMovieDB.privat = true
+        }
+    },
+    writeYourGengers: function() {
+        for (let i = 1; i < 4; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}?`,'')
+
+            if (genre == null || genre == '') {
+                i--
+            } else {
+                personalMovieDB.genres[i - 1] = genre
+            }
+        }
+        personalMovieDB.genres.forEach((elem, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${elem}`)
+        })
     }
 };
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        alert('Вы киноман');
-    } else {
-        alert('Произошла ошибка')
-    }
-
-};
-
-detectPersonalLevel();
-
-function writeYourGengers() {
-    for (let i = 1; i < 4; i++) {
-        let questionGenger = prompt(`Ваш любимый жанр под номером ${i}?`,'')
-
-        personalMovieDB.genres.push(questionGenger)
-    }
-};
-
-writeYourGengers();
-
-function showMyDB(obj) {
-    if (obj.privat == false) {
-        console.log(personalMovieDB);
-    } else {
-        console.log(`База данных скрыта для вас!`)
-    }
-};
-
-showMyDB(personalMovieDB);
 
 
